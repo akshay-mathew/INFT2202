2906e4db2bee7de606dccda170cc2899f601f036
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2025-01-10 14:31:31 -0500

    folders

---
+++ ice/src/client/app/animal/search.js
@@ -0,0 +1,7 @@
+/*
+    Name: Akshay Mathew
+    filename: search.js
+    Course: INFT 2202
+    Date: January 10
+    Description: This is my general style sheet.  Styles that affect all pages should go here.
+*/
---
+++ ice/src/client/app/app.js
@@ -0,0 +1,7 @@
+/*
+    Name: Akshay Mathew
+    filename: app.jss
+    Course: INFT 2202
+    Date: January 10
+    Description: This is my general style sheet.  Styles that affect all pages should go here.
+*/
