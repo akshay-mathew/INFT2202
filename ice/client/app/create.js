10999285f62dca7de0f6c209a6dbb7231e0ce82e
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2025-01-17 14:27:36 -0500

    edits

---
+++ ice/client/app/create.js
@@ -0,0 +1,65 @@
+/*
+    Name: Akshay Mathew
+    filename: add.js
+    Course: INFT 2202
+    Date: January 10
+    Description: This is my general style sheet.  Styles that affect all pages should go here.
+*/
+function validateAnimalForm(form){
+    let isValid = true;
+
+    ['name','breed','legs','eyes','sound'].forEach((fieldName)=>{
+        const field = form[fieldName];
+        const errorMessage = field.nextElementSibling;
+        if (!field.value.trim()||(['legs','eyes'].includes(fieldName)&& parseInt(field.value)<=0))
+        {
+            errorMessage.textContent = `Please provide a valid ${fieldName}.`;
+            errorMessage.classList.remove('d-none');
+            field.classList.add('is-invalid');
+            isValid=false;
+            
+
+        }else{
+            errorMessage.textContent= '';
+            errorMessage.classList.add('d-none');
+            field.classList.remove('is-invalid');
+            field.classList.add('is-valid');
+        }
+    });
+
+    return isValid;
+}
+
+function submitAnimalForm(event)
+{
+    event.preventDefault();
+
+    const form = event.target;
+    const isValid = validateAnimalForm(form);
+
+    if(isValid)
+    {
+        const animal= {
+            name:form.name.value.trim(),
+            breed:form.breed.value.trim(),
+            legs:parseInt(form.legs.value),
+            eyes:parseInt(form.eyes.value),
+            sound:form.sound.value.trim(),
+        };
+        console.log('Animal saved:',animal);
+
+        form.reset();
+        ['name','breed','legs','eyes','sound'].forEach((fieldName)=>
+        {
+           const field = form[fieldName];
+           field.classList.remove('is-valid')
+        });
+    }else{
+        const messageBox = document.getElementById('message-box');
+        messageBox.textContent = 'Please fix the errors';
+        messageBox.classList.remove('d-none');
+    }
+}
+
+const animalForm = document.getElementById('animal-form');
+animalForm.addEventListener('submit',submitAnimalForm);
