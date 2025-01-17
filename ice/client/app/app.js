e2d44fc04d3aa1d18f3b0dbfb6b825726a2aa7e2
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2025-01-17 13:45:26 -0500

    Update app.js

--- ice/client/app/app.js
+++ ice/client/app/app.js
@@ -1,6 +1,6 @@
 /*
     Name: Akshay Mathew
-    filename: app.jss
+    filename: app.js
     Course: INFT 2202
     Date: January 10
     Description: This is my general style sheet.  Styles that affect all pages should go here.
