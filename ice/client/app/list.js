37a15192c9a044f6df8031e82587cb860ff5ab23
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2025-01-24 13:12:39 -0500

    Update list.js

--- ice/client/app/list.js
+++ ice/client/app/list.js
@@ -1,13 +1,11 @@
 
-/*
+/*--
     Name: Akshay Mathew
     filename: list.js
     Course: INFT 2202
     Date: January 17
     Description: This is my general style sheet.  Styles that affect all pages should go here.
-*/
-
-
+--*/
 
 
 console.log('we are on the list page');
