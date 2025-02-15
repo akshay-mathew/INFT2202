278a42308828acc99c5fd0270eac5abbb4d6efed
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2025-02-15 00:40:08 -0500

    edits applied

--- assignment1/src/client/app/app.js
+++ assignment1/src/client/app/app.js
@@ -1,7 +1,7 @@
-// Name: Your Name
+// Name: Akshay Mathew
 // Course: INFT 2202
-// Date: Today's Date
-// Description: Common functions for all pages.
+// Date: 2025-02-15
+// Description: This is a page for my shop.
 
 // Set the current year in the footer
 document.getElementById('copyrightYear').textContent = new Date().getFullYear();
